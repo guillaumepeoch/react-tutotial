6728712ca509b8a4d1eb3876354d5b80d1ba1a86
guillaume <guillaume.peoch@gmail.com>
2018-07-16 22:41:23 -0700

    First push
    create react app

---
+++ hangman-game/hangman/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  generateWord (){
+    possibli
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Hangman Game</h1>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
