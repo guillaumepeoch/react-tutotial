ec970e0890a5c355f4f475455e9b0655904ea2ed
guillaume <guillaume.peoch@gmail.com>
2018-07-25 11:27:29 -0700

    React Uncontrolled forms

--- react-form/src/components/uncontrolled.js
+++ react-form/src/components/uncontrolled.js
@@ -6,12 +6,39 @@ class Uncontrolled extends Component {
 
     }
 
+
+    handleSubmitButton = (e) => {
+      e.preventDefault();
+      const value = {
+        name:this.name.value,
+        lastName:this.lastName.value
+      }
+    }
+
+    onshandler(e){
+      e.preventDefault();
+    }
+
     render(){
-        return(
-            <div>
-                Uncontrolled
-            </div>
-        )
+      return(
+        <form onSubmit={this.onshandler}>
+          <label>Name</label>
+          <div>
+            <input
+              type="text"
+              ref={ input => this.name = input }
+            />
+          </div>
+          <label>Last Name</label>
+          <div>
+            <input
+              type="text"
+              ref={ input => this.lastName = input }
+            />
+          </div>
+          <button onClick={this.handleSubmitButton}> Sign In</button>
+        </form>
+      )
     }
 }
 
