08a4851f675e8fa111d35d4c9f6731b24074a6da
guillaume <guillaume.peoch@gmail.com>
2018-08-02 10:36:03 -0700

    1 - Setup
    - Connect Redux with React
    - Now need Actions and Reducers

--- basic-redux/src/index.js
+++ basic-redux/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// 1 - Need to wrappe the main app with the Provider
+import { Provider } from 'react-redux';
+// 2 - Need to create the store to apply it to the provider
+import { createStore, applyMiddleware } from 'redux';
 
 const App = function(){
   return (
@@ -9,4 +13,21 @@ const App = function(){
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create the function that binds the createStore with the middleware
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(
+  // Connect Redux with React :
+  <Provider store={createStoreWithMiddleware()}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
+
+
+
+/*
+
+APP => SERVER => REDUX => MIDDLEWARE => Component.js => APP
+
+
+*/
